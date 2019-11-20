9a1d7eac8cc9ff66a766a79f7a6a3256c3122660
Reinhard Liess <reinhard.liess@gmail.com>
2019-11-20 15:58:42 +0100

    add new book route, view

--- routes/books.js
+++ routes/books.js
@@ -16,12 +16,33 @@ function asyncHandler(cb) {
   }
 }
 
-/* GET listing of all books */
+// GET listing of all books
 router.get('/', asyncHandler(async (req, res) => {
   const books = await Book.findAll({
     order: [["title", "ASC"]]
   });
   res.render("index", { books, title: "Books" });
+}));
+
+// GET Add a new book form
+router.get('/new', (req, res) => {
+  res.render("new-book", { book: {}, title: "New Book" });
+});
+
+// POST create a new book
+router.post('/new', asyncHandler(async (req, res) => {
+  try {
+    const book = await Book.create(req.body);
+    res.redirect('/');
+  } catch (error) {
+    // checking the error
+    if (error.name === "SequelizeValidationError") {
+      const book = await Book.build(req.body);
+      res.render("new-book", { book, errors: error.errors, title: "New Book" })
+    } else {
+      throw error;
+    }
+  }
 }));
 
 module.exports = router;
