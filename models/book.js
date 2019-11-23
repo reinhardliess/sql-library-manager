4a99ee41d16e8dfe122e5bf35387c30ad5fbb4c6
Reinhard Liess <reinhard.liess@gmail.com>
2019-11-23 17:03:43 +0100

    fix validation for books.year

--- models/book.js
+++ models/book.js
@@ -26,7 +26,7 @@ module.exports = (sequelize) => {
       type: Sequelize.INTEGER,
       validate: {
         customValidator(value) {
-          if (value.trim() && !value.match(/\d{1,4}/)) {
+          if (value.trim() && !value.match(/^\d{1,4}$/)) {
             throw new Error('"Year" must be between 1 and 4 digits and numeric');
           }
         }
